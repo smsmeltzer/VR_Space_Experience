e2f6ab1d5b7a3f1d936b4900208fc292823d989d
Ryan Parmer <ryan.parmer.22@gmail.com>
2024-11-21 00:00:05 -0600

    joystick reference

--- Assets/JoystickController.cs
+++ Assets/JoystickController.cs
@@ -8,17 +8,41 @@ public class JoystickController : XRGrabInteractable
     // Start is called before the first frame update
     public Vector3 startPos;
     public Vector3 currPos;
+    public bool grabbed;
     protected override void Awake()
     {
         base.Awake();
-        startPos = transform.position;
-        currPos = transform.position;
+        startPos = transform.localPosition;
+        //currPos = transform.localPosition;
+        grabbed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currPos = transform.position;
+        //currPos = transform.localPosition;
+        if (grabbed)
+        {
+            Move();
+        }
 
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        grabbed = true;
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        grabbed = false;
+        transform.localPosition = startPos;
+        base.OnSelectExited(args);
+    }
+
+    private void Move()
+    {
+        
     }
 }
