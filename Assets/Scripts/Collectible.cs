9179f71e0c0a9e01f5307c7545ba8367bf8562d2
Lucas-Vaughan <lucas@steeledestinies.com>
2024-11-26 10:26:49 -0600

    Final collectibles demo

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -7,10 +7,16 @@ using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 public class Collectible : MonoBehaviour
 {
 
+    public Vector3 initialAngularVelocity = new Vector3(0.5f, 1f, 0.33f); // Define angular velocity in the Inspector
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.angularVelocity = initialAngularVelocity;
+        }
     }
 
     // Update is called once per frame
